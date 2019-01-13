d4503b98655eb3dd74a2a2cbc275407232556520
Nigarkhan703 <heenaperween703@gmail.com>
2019-01-13 16:27:36 +0530

    Indo Component Pages and one images of Indofork banner which is in top.

--- OMPhoenix-SPA/src/app/indo/indo.component.spec.ts
+++ OMPhoenix-SPA/src/app/indo/indo.component.spec.ts
@@ -22,7 +22,7 @@ describe('IndoComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+    it('should create', () =>  {
     expect(component).toBeTruthy();
   });
 });
--- OMPhoenix-SPA/src/app/indo/indo.component.ts
+++ OMPhoenix-SPA/src/app/indo/indo.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-indo',
-  templateUrl: './indo.component.html',
+  templateUrl: './indo.component.html' ,
   styleUrls: ['./indo.component.css']
 })
 export class IndoComponent implements OnInit {
